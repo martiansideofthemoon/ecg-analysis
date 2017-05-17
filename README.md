fa6d0e013d731b98db6b19c425e4ddba6ac0a782
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2017-05-17 21:18:50 +0530

    Adding tSNE plotter

---
+++ README.md
@@ -0,0 +1,6 @@
+## ECG Analysis
+* `data/scrape.py` - Downloads the entire PTB dataset.
+* `data/patients.txt` - A list of all the records.
+* `separate.py` - Split the data into positive cases and negative cases.
+* `tsne_plot.py` - Extract coefficents from the waveforms and plot the t-SNE output.
+* `tsne.py` - A Python implementation of [t-SNE](https://lvdmaaten.github.io/tsne/), written by the original author.
---
+++ tsne_plot.py
@@ -0,0 +1,54 @@
+"""This generates the vectors."""
+import glob
+import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import random
+
+from pywt import wavedec
+from tsne import tsne
+
+np.random.seed(0)
+
+files = glob.glob("final_positive/*.csv")
+outputs = []
+labels = []
+print len(files)
+for file1 in files:
+    data = np.genfromtxt(file1, delimiter=",")
+    samples = data[:, 1]
+    output = wavedec(samples, wavelet='db8', level=4)
+    output = np.concatenate((output[0], output[1], output[2]))
+    outputs.append(output)
+    labels.append(0)
+
+files = glob.glob("final_negative/*.csv")
+
+for file1 in files:
+    data = np.genfromtxt(file1, delimiter=",")
+    samples = data[:, 1]
+    output = wavedec(samples, wavelet='db8', level=4)
+    output = np.concatenate((output[0], output[1], output[2]))
+    outputs.append(output)
+    labels.append(1)
+
+c = list(zip(outputs, labels))
+random.shuffle(c)
+outputs, labels = zip(*c)
+
+outputs = np.array(outputs)
+labels = np.array(labels)
+
+# Uncomment below to generate random data
+# outputs = np.random.rand(len(labels), 100)
+
+final = tsne(outputs, no_dims=2)
+
+# Use the following snippet for 3-D plots
+# fig = pylab.figure()
+# ax = Axes3D(fig)
+# ax.scatter(final[:, 0], final[:, 1], final[:, 2], c=labels)
+# plt.show()
+
+plt.scatter(final[:, 0], final[:, 1], c=labels, marker='o')
+plt.show()
